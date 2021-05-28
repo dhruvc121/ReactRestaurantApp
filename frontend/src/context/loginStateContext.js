5c658e2bb3d4325df83606dd172a7e45f0166762
Dhruv Champaneri <dhruvc121@gmail.com>
2021-05-28 12:08:50 +0530

    post deploy bug fix -1

--- frontend/src/context/loginStateContext.js
+++ frontend/src/context/loginStateContext.js
@@ -3,7 +3,7 @@ import React,{useState} from 'react';
 export const LoginStateContext=React.createContext()
 
 const LoginStateContextProvider=(props)=>{
-		const [loginState,setLoginState]=useState(true)
+		const [loginState,setLoginState]=useState(false)
 		return(
 			<LoginStateContext.Provider value={[loginState,setLoginState]}>
 			{props.children}
